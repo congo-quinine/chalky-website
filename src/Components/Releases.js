1643d43193e7e81d77b7edd30566b6188990dba8
congo-quinine <congo.quinine@gmail.com>
2019-01-09 14:59:34 +0000

    tweaks

--- src/Components/Releases.js
+++ src/Components/Releases.js
@@ -28,7 +28,7 @@ class Releases extends Component {
                    allow="autoplay"
                    src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/530823246&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
                   </iframe>
-                  <h5 className='date-line'>-Released December 7, 2018 </h5>
+                  <h5 className='date-line'>-Released December 7, 2018</h5>
                     <p>
                       Chalky &amp; Seppa have created something truly unique with “Bright Spots” — combining chunky rhythms of hip-hop with raw instrumentalism and involving hooks, the duo takes you on a journey through their combined influences, pulling the past into the future. To top this off, we’re excited to be presenting Chalky’s music to the world. Make sure you keep tabs on him in time to come, as his musical output is nothing short of astonishing!
                     </p>
